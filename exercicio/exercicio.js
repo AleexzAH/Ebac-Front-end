a929e08a07e1fad0543fd71d0cfe3dc01df4e931
Alexander Henrique <henriquealexander60@gmail.com>
2026-03-10 20:59:35 -0300

    Primeiro comit

---
+++ exercicio/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiplicacao(a, b) {
+    const resultado = a * b;
+    console.log(`A multiplicação de ${a} com ${b} é ${resultado}`);
+}
+function saudacao(nome) {
+    console.log(`Olá ${nome}. Seja bem vindo`);
+}
+multiplicacao(5, 54);
+saudacao('Alexander');
+//# sourceMappingURL=exercicio.js.map
---
+++ exercicio/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number){
+    const resultado: number = a * b;
+    console.log(`A multiplicação de ${a} com ${b} é ${resultado}`);
+}
+
+function saudacao(nome: string){
+    console.log(`Olá ${nome}. Seja bem vindo`);
+}
+
+multiplicacao(5, 54);
+saudacao('Alexander');
